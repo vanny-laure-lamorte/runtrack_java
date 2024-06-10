3fcefeecc8aa1a8e3cff4c7161a3aef7c9bcbe03
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-10 20:25:05 +0200

    Jour01, job4,déclarer des variables sur une ligne

--- jour01/job04/DeclarationVariable.java
+++ jour01/job04/DeclarationVariable.java
@@ -2,10 +2,8 @@ package jour01.job04;
 
 public class DeclarationVariable {
     public static void main(String[] args) {
-        int a = 0;
-        int b = 0; 
-        int c = 0; 
-        
+        int a = 0, b = 0, c = 0; 
+
         System.out.println("a = " + a);
         System.out.println("b = " + b);
         System.out.println("c = " + c);
