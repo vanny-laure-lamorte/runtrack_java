a04d071ee38e23ce1a0c7e52808237f820b3b4f2
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-10 20:23:38 +0200

    Jour01, job4,déclarer des variables

---
+++ jour01/job04/DeclarationVariable.java
@@ -0,0 +1,13 @@
+package jour01.job04;
+
+public class DeclarationVariable {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0; 
+        int c = 0; 
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
