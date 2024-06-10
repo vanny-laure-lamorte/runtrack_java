ee124d077962acc2ca41b0581603cd11e02fbc47
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-10 20:29:44 +0200

    Jour01, job5, réaliser des calculs

---
+++ jour01/job05/Calcul.java
@@ -0,0 +1,16 @@
+package jour01.job05;
+
+public class Calcul {
+    public static void main(String[] args) {
+
+        int nombre =7; 
+        System.out.println("a = " + nombre);
+
+        nombre = nombre+3; 
+        System.out.println("a = " + nombre);
+
+        nombre = nombre *5; 
+        System.out.println("a = " + nombre);
+    }
+    
+}
