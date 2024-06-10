39a104b1cc499feafe8baff9036e7647f09bd69c
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-10 19:28:36 +0200

    Jour01, job3, créer des commentaires

---
+++ jour01/job03/HelloWorld.java
@@ -0,0 +1,16 @@
+package jour01.job03;
+
+// Commentaire sur une ligne
+
+/* Commentaire 
+sur 
+plusieurs 
+lignes 
+ */
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
