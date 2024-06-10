3047fce5c51447a739a902e2f3e18acb38a0697a
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-10 21:33:05 +0200

    Jour01, job10, récupérer la saisie d'un utilisateur

---
+++ jour01/job10/Input.java
@@ -0,0 +1,18 @@
+package jour01.job10;
+
+import java.util.Scanner;
+
+public class Input {
+    
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Veuillez saisir votre prénom :");
+        String prenom = input.nextLine();   
+        System.out.println("Veuillez saisir votre Nom :");
+        String nom = input.nextLine();
+        input.close();
+
+        System.out.println("Votre nom est: " + prenom + " " + nom);       
+    }
+}
