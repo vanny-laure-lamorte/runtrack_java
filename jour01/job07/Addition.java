7d282543d2f35857c4c2aaf393bae92683a500a3
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-10 20:47:33 +0200

    Jour01, job7, faire une division

---
+++ jour01/job07/Addition.java
@@ -0,0 +1,12 @@
+package jour01.job07;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 73;
+
+       float resultat = (float)b/a;
+        System.out.println(resultat);
+    }
+    
+}
