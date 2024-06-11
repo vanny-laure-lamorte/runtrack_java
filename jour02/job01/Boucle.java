d82f85eb13b5f9ad2315f5699691a24064471dfd
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-11 10:23:09 +0200

    Jour02, job01, compter jusqu'à 100

---
+++ jour02/job01/Boucle.java
@@ -0,0 +1,9 @@
+package jour02.job01;
+
+public class Boucle {
+    public static void main(String[] args) {
+        for (int i =1; i <101; i++){
+            System.out.println(i);
+        }
+    }
+}
