a895cc4d896255ea0113b10e91773d1c89a16df6
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-11 13:34:30 +0200

    Jour02, job12, combinaison de dés

---
+++ jour02/job12/Boucle.java
@@ -0,0 +1,22 @@
+package jour02.job12;
+public class Boucle{
+
+    public static void main(String[] args) {
+        int[] dice = {1, 2, 3, 4, 5, 6};
+        int totalCombinations = 0;
+
+        System.out.println("Possible combinations with three dice:");
+
+        // Loops to generate all combinations
+        for (int d1 : dice) {
+            for (int d2 : dice) {
+                for (int d3 : dice) {
+                    System.out.println(d1 + " " + d2 + " " + d3);
+                    totalCombinations++;
+                }
+            }
+        }
+
+        System.out.println("Total number of combinations: " + totalCombinations);
+    }
+}
