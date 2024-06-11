bcfa476ba93e54fb2391ec5e5d990cdef76dcb12
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-11 10:52:28 +0200

    Jour02, job04, afficher les chiffres pairs de 0 à 100

---
+++ jour02/jour04/Boucle.java
@@ -0,0 +1,11 @@
+package jour02.jour04;
+
+public class Boucle {
+    public static void main(String[] args) {
+        for (int i=0; i<=100; i++) {
+            if (i%2 == 0 && i !=0) {
+                System.out.print(i + " " );
+            }
+        }
+    }
+}
