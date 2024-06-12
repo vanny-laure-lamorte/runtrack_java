3164941f7a7ac27e82c13fdfaec336734ae97a86
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-12 10:21:43 +0200

    Jour03, job03, créer un tableau avec des str

--- jour03/job03/Tableau.java
+++ jour03/job03/Tableau.java
@@ -2,7 +2,20 @@ package jour03.job03;
 
 public class Tableau {
     public static void main(String[] args) {
-        
+
+        // Initialisation du tableau
+       String []Nom = {"Josette", "John","Myrtille", "Marc"}; 
+
+       // Afficher le prénom John
+       System.out.println(Nom[1]);
+
+       // Modifier la valeur du tableau
+       Nom[2] = "Mireille";
+
+       // Afficher le tableau après changements
+       System.out.print("Les prénoms du tableau: ");
+       for(String noms: Nom) {        
+        System.out.print(noms + " ");
+       }
     }
-    
 }
