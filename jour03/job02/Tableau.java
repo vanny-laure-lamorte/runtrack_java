f3a6bfd00b9f4e8860ec688b9238f13a3323c257
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-12 10:11:27 +0200

    Jour03, job03, modifier les éléments d'un tableau

---
+++ jour03/job02/Tableau.java
@@ -0,0 +1,29 @@
+package jour03.job02;
+
+public class Tableau {
+    public static void main(String[] args) {
+
+        // Initialisation du tableau
+        int []monTableau = {12,6, 76, 89};       
+
+        // Afficher les éléments du tableau
+        System.out.print("Tableau avant changement: ");
+        for (int i=0; i< monTableau.length; i++){
+            System.out.print(monTableau[i] + " ");
+        }
+
+        // Nouvelle valeur du tableau
+        monTableau[0] = 1; 
+        monTableau[1] = 2; 
+        monTableau[2] = 3; 
+        monTableau[3] = 4;        
+
+        // Afficher les nouveaux éléments du tableau
+        System.out.println();
+        System.out.print("Tableau après changement: ");
+        for (int i=0; i< monTableau.length; i++){
+            System.out.print(monTableau[i] + " ");
+        }
+    }
+    
+}
