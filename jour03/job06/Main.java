93d76d001808f479c2cab54ebd31d14977eccadc
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-12 11:28:40 +0200

    Jour03, job06, faire une tableau de 10 entiers aléatoirs

---
+++ jour03/job06/Main.java
@@ -0,0 +1,23 @@
+package jour03.job06;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Tableau de 10 entiers
+        int[] tableau = new int[10];
+
+        // Remplissage le tableau de façon random
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            tableau[i] = random.nextInt(100); // Nombres aléatoires entre 0 et 99
+        }
+
+        // Affichage le tableau
+        System.out.print("Tableau de nombres randoms: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(tableau[i] + " ");
+        }
+    }
+}
