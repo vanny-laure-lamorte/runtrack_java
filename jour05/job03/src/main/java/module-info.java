b510216946c5df329e959055fd01c4031f93e3d7
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-17 12:40:53 +0200

    Jour05, job03, apprendre a utiliser FlowLayout

--- jour05/job03/src/main/java/module-info.java
+++ jour05/job03/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.job03 {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires org.controlsfx.controls;
 
     opens com.example.job03 to javafx.fxml;
     exports com.example.job03;
